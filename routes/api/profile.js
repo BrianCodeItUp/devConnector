3cbebfcdab8d9beccf99a0b40811a199b5a66c8e
Brian <wl00387719@gmail.com>
2019-06-23 21:56:57 +0800

    add route: get all profiles, get profile by id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -7,6 +7,42 @@ const User = require('../../models/User')
 const Profile = require('../../models/Profile')
 const fieldsConfig = require('../fieldsConfig')
 const utils = require('../utils')
+
+/**
+ * @route  Get api/profile
+ * @desc   Get all profiles
+ * @access Public
+ */
+router.get('/', async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate('user', ['name', 'avatar'])
+    res.json(profiles)
+  } catch (e) {
+    console.error('Error occured at get all profiles', e)
+  }
+})
+
+/**
+ * @route  Get api/profile/user/:user_id
+ * @desc   Get profile by user ID
+ * @access Public
+ */
+router.get('/user/:user_id', async (req, res) => {
+  try {
+    const profile = await Profile
+      .findOne({ user: req.params.user_id })
+      .populate('user', ['name', 'avatar'])
+
+    if (!profile) return res.status(400).json({ msg: 'Profile Not found' })
+    res.json(profile)
+  } catch (e) {
+    console.error('Error occured at get all profiles', e)
+    e.kind === 'ObjectId'
+      ? res.status(400).json({ msg: 'Profile Not found' })
+      : res.status(500).json({ msg: 'Server Error' })
+  }
+})
+
 /**
  * @route  Get api/profile/me
  * @desc   Get current users profile
@@ -16,7 +52,8 @@ router.get('/me', auth, async (req, res) => {
   try {
     const profile = await Profile
       .findOne({ user: req.user.id })
-      .populate('user').populate('user', ['name', 'avatar'])
+      .populate('user')
+      .populate('user', ['name', 'avatar'])
 
     if (!profile) return res.status(400).json({ msg: 'There is no profile for this user' })
     res.json(profile)
